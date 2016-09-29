320124d5e9c942ea41de309f91c99644241ce1b1
matt bonsall <matt.bonsall.v@gmail.com>
2016-09-29 10:01:06 -0500

    Update Beehives.py
    https://open.kattis.com/problems/beehives

--- Beehives.py
+++ Beehives.py
@@ -1,24 +1,27 @@
-import sys
-
 import math
-
-# STILL WIP (BEDTIME)
 
 def dist(x1, y1, x2, y2, expected):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) <= expected
 
 
-for input in sys.stdin:
-    a = float(input.split()[0])
-    b = int(input.split()[1])
-    x = []
-    y = []
-    if a == 0:
-        exit()
-    for i in range(0, b):
-        hive = raw_input()
-        c, d = [float[n] for n in hive.split()]
-        x.append(c , d , True)
-        print x
-print x
+for useless in range(10):
+	a, b = raw_input().split()
+	a = float(a)
+	b = int(b)
+	if b == 0:
+		break
+	x = []
+	y = []
+	z = []
+	for i in range(b):
+		xt, yt = raw_input().split()	
+		z.append(True)
+		for r, u in enumerate(x):
+			if(dist(float(xt) , float(yt) , u, y[r], a)):
+				z[i] = False
+				z[r] = False		
+		x.append(float(xt))
+		y.append(float(yt))
+		
+	print b - sum(z), "sour,", sum(z), "sweet"
 
